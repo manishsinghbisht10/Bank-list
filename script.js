05d1b8bbdab5a75a399f7da4ad1789b9e266c7a4
Manish bisht <manishsinghbisht10@gmail.com>
2024-04-14 10:03:35 +0530

    smooth scrolling

--- script.js
+++ script.js
@@ -28,3 +28,14 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
